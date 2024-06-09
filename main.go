93c1b397136a70b19884ebcc853040f8ddd3c8e5
nikhilcad <nikhilcaddilac@gmail.com>
2024-06-09 22:20:16 +0530

    A bubbletea example

--- main.go
+++ main.go
@@ -1,92 +1,113 @@
 package main
 
 import (
-	// "fmt"
-	"net/http"
-	"github.com/gin-gonic/gin"
-	"github.com/mmcdole/gofeed"
+    "fmt"
+    "os"
+
+    tea "github.com/charmbracelet/bubbletea"
 )
 
-// struct fields need to be uppercase, json field lowercase is just preference
-type item struct {
-	Title       string `json:"title"`
-	Link        string `json:"link"`
-	Description string `json:"description"`
-	Image string `json:"image"`
+type model struct {
+    choices  []string           // items on the to-do list
+    cursor   int                // which to-do list item our cursor is pointing at
+    selected map[int]struct{}   // which to-do items are selected
 }
 
-type feed struct{
-	Title string `json:"title"`
-	Description string `json:"description"`
-	Link string `json:"link"`
-	Items []item `json:"items"`
-}
+func initialModel() model {
+	return model{
+		// Our to-do list is a grocery list
+		choices:  []string{"Buy carrots", "Buy celery", "Buy kohlrabi"},
 
-var feeds = populateFeeds()
-
-func populateFeeds() []feed {
-
-	urls := []string{
-		"http://feeds.twit.tv/twit.xml",
-		"https://rss.nytimes.com/services/xml/rss/nyt/World.xml",
+		// A map which indicates which choices are selected. We're using
+		// the  map like a mathematical set. The keys refer to the indexes
+		// of the `choices` slice, above.
+		selected: make(map[int]struct{}),
 	}
-
-	var feeds []feed
-
-	var fp = gofeed.NewParser()
-
-	
-	for _, url := range urls {
-
-		var feedParsed, _ = fp.ParseURL(url)
-
-		newFeed := feed{
-			Title:       feedParsed.Title,
-			Description: feedParsed.Description,
-			Link:        feedParsed.Link,
-		}
-
-		for _, parsedItem := range feedParsed.Items {
-			
-			imgUrl := ""
-			if parsedItem.Image != nil && parsedItem.Image.URL != "" {
-				imgUrl = parsedItem.Image.URL
-			}
-
-			newItem := item{
-				Title:       parsedItem.Title,
-				Link:        parsedItem.Link,
-				Description: parsedItem.Description,
-				Image: imgUrl,
-			}
-
-			newFeed.Items = append(newFeed.Items, newItem)
-		}
-
-		feeds = append(feeds, newFeed)
-	}
-
-	return feeds
-
 }
 
-func getFeeds(context *gin.Context){
-	context.IndentedJSON(http.StatusOK, feeds)
+func (m model) Init() tea.Cmd {
+    // Just return `nil`, which means "no I/O right now, please."
+    return nil
 }
 
-func createFeed(c *gin.Context) {
-	var newFeed feed
-	if err := c.BindJSON(&newFeed); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-	feeds = append(feeds, newFeed)
-	c.IndentedJSON(http.StatusCreated, newFeed)
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+    switch msg := msg.(type) {
+
+    // Is it a key press?
+    case tea.KeyMsg:
+
+        // Cool, what was the actual key pressed?
+        switch msg.String() {
+
+        // These keys should exit the program.
+        case "ctrl+c", "q":
+            return m, tea.Quit
+
+        // The "up" and "k" keys move the cursor up
+        case "up", "k":
+            if m.cursor > 0 {
+                m.cursor--
+            }
+
+        // The "down" and "j" keys move the cursor down
+        case "down", "j":
+            if m.cursor < len(m.choices)-1 {
+                m.cursor++
+            }
+
+        // The "enter" key and the spacebar (a literal space) toggle
+        // the selected state for the item that the cursor is pointing at.
+        case "enter", " ":
+            _, ok := m.selected[m.cursor]
+            if ok {
+                delete(m.selected, m.cursor)
+            } else {
+                m.selected[m.cursor] = struct{}{}
+            }
+        }
+    }
+
+    // Return the updated model to the Bubble Tea runtime for processing.
+    // Note that we're not returning a command.
+    return m, nil
 }
+
+
+func (m model) View() string {
+    // The header
+    s := "What should we buy at the market?\n\n"
+
+    // Iterate over our choices
+    for i, choice := range m.choices {
+
+        // Is the cursor pointing at this choice?
+        cursor := " " // no cursor
+        if m.cursor == i {
+            cursor = ">" // cursor!
+        }
+
+        // Is this choice selected?
+        checked := " " // not selected
+        if _, ok := m.selected[i]; ok {
+            checked = "x" // selected!
+        }
+
+        // Render the row
+        s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+    }
+
+    // The footer
+    s += "\nPress q to quit.\n"
+
+    // Send the UI for rendering
+    return s
+}
+
 
 func main() {
-	router := gin.Default()
-	router.GET("/feeds", getFeeds)
-	router.POST("/feeds", createFeed)
-	router.Run("localhost:8090")
+    p := tea.NewProgram(initialModel())
+    if _, err := p.Run(); err != nil {
+        fmt.Printf("Alas, there's been an error: %v", err)
+        os.Exit(1)
+    }
 }
